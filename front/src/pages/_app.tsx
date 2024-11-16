8b00d9cd106cd24dc9dcdb6272da3ad40c441334
Oleg Melnik <oleg.mel123@gmail.com>
2024-11-16 11:16:52 +0200

    Disable pointer events on blured file

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,31 +1,17 @@
-import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
-import Script from 'next/script';
-import Layout from '../components/layout/layout';
-import Modal from 'react-modal';
+import React from 'react';
 
-Modal.setAppElement('#__next');
+export function App() {
 
-export default function App( { Component, pageProps }: AppProps ) {
-  return <>
-    <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-P3ZWGZ54ZN" />
-    <Script
-      id='google-analytics'
-      strategy="afterInteractive"
-      dangerouslySetInnerHTML={{
-        __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-P3ZWGZ54ZN', {
-            page_path: window.location.pathname,
-          });
-        `,
-      }} />
-      
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  </>
-  return
+  const sayHello = () => {
+    return 'Hello!';
+  }
+
+  return (
+    <div> // simple HTML tags
+
+      // call JS func in HTML
+      <span>{sayHello()}</span>
+
+    </div>
+  )
 }
